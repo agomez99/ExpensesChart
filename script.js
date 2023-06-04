215d6d022645738e2719563074fe7cd2aebd28d8
Austine Gomez <austine.gomez@yahoo.com>
2023-06-03 21:17:22 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'],
+    datasets: [{
+    label: '$',
+      data: [17.45, 34.91,  52.36, 31.07,23.39,  43.28,  25.48],
+      borderWidth: 1,
+      backgroundColor: [
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(186, 34%, 60%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)',
+        'hsl(10, 79%, 65%)'
+      ],
+      borderRadius: 10,
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        display: false,
+    }
+    }
+  }
+});
+
+
+const labels = Utils.days({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [17.45, 34.91,  52.36, 31.07,23.39,  43.28,  25.48],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
