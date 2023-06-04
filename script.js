087ce44cddba400600c00acdf5576fa63d912c89
Austine Gomez <austine.gomez@yahoo.com>
2023-06-04 01:32:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
 const ctx = document.getElementById('myChart');
-
 new Chart(ctx, {
   type: 'bar',
   data: {
     labels: ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'],
-
     datasets: [{
     label: '$',
       data: [17.45, 34.91,  52.36, 31.07,23.39,  43.28,  25.48],
@@ -20,9 +18,7 @@ new Chart(ctx, {
         'hsl(10, 79%, 65%)'
       ],
       hoverBackgroundColor: ['hsl(9, 68%, 73%)', 'hsl(9, 68%, 73%)', 'hsl(185, 42%, 79%)', 'hsl(9, 68%, 73%)', 'hsl(9, 68%, 73%);', 'hsl(9, 68%, 73%)', 'hsl(9, 68%, 73%)'],
-
       borderRadius: 5,
-
     }]
   },
   options: {
